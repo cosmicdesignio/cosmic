26dc881fd2c7dcd7e775e894e435a392ddc145ca
Cosmic <154437292+cosmicdesignio@users.noreply.github.com>
2024-01-10 01:25:42 +0000

    Update barba.js

--- barba.js
+++ barba.js
@@ -3,15 +3,33 @@ barba.init({
     {
       name: "fade-transition",
       leave(data) {
-        // Add your animation timeline using GSAP here
+        gsap.to(".navbar-header_link", {
+          opacity: 0,
+          duration: 1,
+          ease: "power4.in",
+        });
+        gsap.to(".link", {
+          opacity: 0,
+          duration: 1,
+          stagger: 0.25,
+          ease: "power4.in",
+        });
         gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
         return gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
       },
       enter(data) {
-        if ("scrollRestoration" in history) {
-          history.scrollRestoration = "manual";
-        }
         // Add your animation timeline using GSAP here
+        gsap.from(".navbar-header_link", {
+          opacity: 0,
+          duration: 1,
+          ease: "power4.in",
+        });
+        gsap.from(".link", {
+          opacity: 0,
+          duration: 1,
+          stagger: 0.25,
+          ease: "power4.in",
+        });
         gsap.from(data.next.container, { opacity: 0, duration: 0.5 });
       },
     },
